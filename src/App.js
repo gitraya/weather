a264f4180a895ee7cddd6a1dded1c236e54a5094
gitraya <mail.raya15@gmail.com>
2021-03-23 17:06:01 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
       </header>
+      <Footer />
     </div>
   );
 }
