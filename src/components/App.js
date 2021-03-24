efab1836d3723fa21083d10f590aff48ba4af7b0
gitraya <mail.raya15@gmail.com>
2021-03-24 08:47:26 +0700

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import Footer from 'components/Footer';
 
 function App() {
+  const cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+  const fetchingData = () => {
+    fetch(`${cors_api_url}https://www.metaweather.com/api/location/1047378`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="App">
-      <div className="container main">
+      <div className="container default">
         <header className="header">
           <nav className="nav">
             <div className="search-button">
-              <button>Search for places</button>
+              <button onClick={fetchingData}>Search for places</button>
             </div>
-            <i className="coordinate-icon"></i>
+            <div className="coordinate-icon">
+              <i class="material-icons-round">my_location</i>
+            </div>
           </nav>
         </header>
         <main className="main">
@@ -17,9 +29,12 @@ function App() {
             <div></div>
           </section>
         </main>
-        <img src="" alt="" />
+        <img
+          src={process.env.PUBLIC_URL + '/images/Cloud-background.png'}
+          alt="cloud background"
+        />
       </div>
-      <div className="container second">
+      <div className="container main">
         <header className="header">
           <nav className="nav">
             <div className="icon-temp"></div>
